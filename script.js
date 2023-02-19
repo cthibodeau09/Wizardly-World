e9870c6615eb1b479bd2c1ae2a8db46d041b17d7
Little Bit <cthibodeau09@gmail.com>
2023-02-18 19:03:38 -0600

    Added comments to script file

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("Hey you clicked the button!")
 
+// password prompts 
+// 1. enter length of password 8 <= 128 
+// 2. ask for character types 
+//    Confirm whether or not to include 
+//      A. Lowercase
+//      B. Uppercase
+//      C. Numeric
+//      D. Special characters
+// 3. Prompt should be validated and a password should be generated that matches criteria 
+// 4. Password is either displayed  in an alert or written on the page 
+
+
   return "Generated password will go here";
 }
 
